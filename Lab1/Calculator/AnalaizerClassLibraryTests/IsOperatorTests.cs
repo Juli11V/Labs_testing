9f8b393d71c293734999ac298ccc4c4f86b915c8
Juli11V <Juli11V>
2022-09-03 22:51:16 +0300

    Lab_1

---
+++ Lab1/Calculator/AnalaizerClassLibraryTests/IsOperatorTests.cs
@@ -0,0 +1,47 @@
+﻿using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AnalaizerClassLibraryTests
+{
+    [TestClass]
+    public class IsOperatorTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient", @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\UnitTests.mdf;Integrated Security=True;Connect Timeout=30", "Test2Data", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void IsOperator_IsOperatorGetFromDB_ResultBoolean()
+        {
+            //Asert
+
+            string operators = (string)TestContext.DataRow["operator"];
+            bool expectedResult = System.Convert.ToBoolean(TestContext.DataRow["result"]);
+
+           //Act
+
+            bool actualResult;
+
+           //Assert
+
+            try
+            {
+                actualResult = AnalaizerClass.IsOperator(operators);
+                Assert.AreEqual(expectedResult, actualResult);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+       
---
+++ Lab1/Calculator/AnalaizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalaizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalaizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1dfdf56-bdb7-446e-b625-a063e4088b90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
