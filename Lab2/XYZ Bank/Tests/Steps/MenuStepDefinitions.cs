f103a0b2b70abc9f14765312a6275cea15a0b71a
Juli11V <Juli11V>
2022-10-25 16:41:55 +0300

    Lab2

---
+++ Lab2/XYZ Bank/Tests/Steps/MenuStepDefinitions.cs
@@ -0,0 +1,19 @@
+﻿using TechTalk.SpecFlow;
+using XYZ_Bank;
+
+namespace Tests.Steps
+{
+    [Binding]
+    public class MenuStepDefinitions : BaseSteps
+    {
+        private MenuPage menuPage;
+
+        [Given(@"I click on the Customers menu item")]
+        public void GivenIClickOnTheCustomersMenuItem()
+        {
+            menuPage = new MenuPage(driver);
+            Thread.Sleep(1000);
+            menuPage.ClickCustomers();
+        }
+    }
+}
---
+++ Lab2/XYZ Bank/XYZ Bank/MenuPage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace XYZ_Bank
+{
+    public class MenuPage : BasePage
+    {
+       public MenuPage(IWebDriver webDriver) : base(webDriver) { }
+
+        private IWebElement btnCustomers => driver.FindElement(By.XPath("//button[@ng-click='showCust()']"));
+
+        public void ClickCustomers() => btnCustomers.Click();
+    }
+}
